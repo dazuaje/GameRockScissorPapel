535d1b74d34865a79425318714bf379806c309a1
dazuaje <davidazuaje1084@gmail.com>
2022-02-20 12:17:37 -0300

    Solucionando bugs

--- app.js
+++ app.js
@@ -17,33 +17,6 @@ const indicador = document.querySelector("#indicador");
 const playAgain = document.querySelector("#playAgain");
 
 
-//let array = ["piedra", "papel", "tijera"];
-
-/*function nombre(parametro)*/ 
-//parametro
-
-
-
-
-/*
-piedra.addEventListener('click', () =>{
-  let random = Math.floor(Math.random() * 3);
-  if(piedra == array[random]){
-    console.log("piedra", array[random]);
-    console.log(random);
-  }else if(papel == array[random]){
-    console.log("papel", array[random]);
-    console.log(random);
-  }else{
-    console.log("tijera", array[random]);
-            console.log(random);
-          }
-    }
-)
-
-
-*/
-
 
 /**********Indicador de victoria o derrota*************/
 let game = document.querySelector('.game');
@@ -51,108 +24,103 @@ let resultado = document.querySelector('#indicador');
 
 
 
+const imgPapel = "./images/icon-paper.svg";
+const imgRock = "./images/icon-rock.svg";
+const imgScissor = "./images/icon-scissors.svg";
 
-/*if(boton seleccionado === tijera || boton selecionado === piedra || boton sleecionad === papel){
-    emapte
-}else if(array === piedra ){
-    */
+const bordes = document.querySelector('#handImage');
 
-/*
-if(tijera && papel || papel && piedra ){
-    Gana
-}
-else if(Seleccion && SeleccionComputadora son iguales){
-    Empata
-}
-else(){
-    Pierde
-}
-
-
-[piedra, papel, tijera];
-
-ramdon = mathFloor()*2
-
-
-if(piedra === )
-
-*/
-
-
-
-
-/*const fuction = (parametro)=>{
-        
-        let random = Math.floor(Math.random() * 3);
-        if(parametro == array[random]){
-            console.log("piedra", array[random]);
-            console.log(random);
-        }else if(parametro == array[random]){
-            console.log("papel", array[random]);
-            console.log(random);
-        }else{
-            console.log("tijera", array[random]);
-            console.log(random);
-        }
-)
-}
-    papel.addEventListener("click",()=>{
-         function ("papel");
-    });
-    
-    piedra.addEventListener("click"){
-        function("piedra");
-    }
-
-    tijera.addEventListener(){
-        function("tijera");
-    }
-*/
 papel.addEventListener("click",()=>{
-    juego("papel");
+    juego("papel", imgPapel);
+    bordes.classList = "handImageContainer boton azul"
+
 });
+
 
 piedra.addEventListener("click",()=>{
-    juego("piedra");
+    juego("piedra", imgRock);
+    bordes.classList = "handImageContainer boton rojo"
+
 });
+
 
 tijera.addEventListener("click",()=>{
-    juego("tijera");
+    juego("tijera", imgScissor);
+    bordes.classList = "handImageContainer boton amarillo";
 });
 
+
 let puntaje = document.querySelector('#puntaje');
+let i = 1;
 
-let i = 0;
-let reset = document.querySelector('.reset');
 
-reset.addEventListener('click', ()=>{
-    i = 0;
-    puntaje.textContent = "0";
+
+/*------------------TOGLE---------- */
+const box = document.querySelector('.box');
+const containerHands = document.querySelector('.containerHands');
+
+
+
+const newGame = document.querySelector('.newGame').addEventListener('click', () =>{
+    box.classList.remove('ocultar');
+    containerHands.classList.remove('show');
+    document.querySelector('.referee');
+    //i = 1;
+    
+
+
 })
 
-function juego(parametro){
-    let array = ["piedra", "papel", "tijera"];
+
+const decision = document.querySelector('#final-decision');
+const computadora = document.querySelector('#handComputer');
+
+
+function juego(parametro, img){
+
+    let array = [imgPapel, imgRock, imgScissor];
 
     let random = Math.floor(Math.random() * 3);
 
+    document.querySelector('#userPickImage').src = img;
+
+    document.querySelector('#computerPickImage').src = array[random];
+    box.classList.add('ocultar');
+    containerHands.classList.add('show');
+
+    
+    if(random == 0){
+        computadora.classList = "handImageContainer boton azul";
+    }else if(random == 1){
+        computadora.classList = "handImageContainer boton rojo";
+    }else{
+        computadora.classList = "handImageContainer boton amarillo";
+    }
     console.log(parametro," ",array[random]);
     
-    if((parametro == "piedra" && array[random] == "tijera") ||
-        (parametro == "papel" && array[random] == "piedra") ||
-        (parametro == "tijera" && array[random] == "papel")){
+    if((parametro == "piedra" && array[random] == imgScissor) || 
+       (parametro == "papel" && array[random] == imgRock) ||
+       (parametro == "tijera" && array[random] == imgPapel)){
         console.log("Ganó");
         puntaje.textContent = i++;
         alert('Ganaste')
+        decision.textContent = 'YOU WIN';
         
-    }else if (parametro == array[random]){
+    }else if ((parametro == "piedra" && array[random] == imgRock) || 
+             (parametro == "papel" && array[random] == imgPapel) ||
+             (parametro == "tijera" && array[random] == imgScissor))
+    {
         console.log("Empató");
         alert('Empate')
+        decision.textContent = 'DRAW'; 
     }else{
         console.log("Perdio");
-        puntaje.textContent = i--;
+        /*puntaje.textContent = i--;*/
         alert('Perdiste')
+        decision.textContent = 'LOSE';
     }
 }
+
 
 
 
